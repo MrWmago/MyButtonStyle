deaed5f1f0a2ad6ad692c9c59eedf9891c1191ed
WangLG <420028932@qq.com>
2017-05-24 15:35:22 +0800

    点击-选择

--- app/src/main/java/com/example/xw/mybuttonstyle/MainActivity.java
+++ app/src/main/java/com/example/xw/mybuttonstyle/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.xw.mybuttonstyle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tv_dianji,tv_xuanze;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        tv_dianji = (TextView) findViewById(R.id.tv_dianji);
+        tv_xuanze = (TextView) findViewById(R.id.tv_xuanze);
+
+        tv_dianji.setOnClickListener(this);
+        tv_xuanze.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_dianji:
+                break;
+            case R.id.tv_xuanze:
+                if (tv_xuanze.isSelected()){
+                    tv_xuanze.setSelected(false);
+                }else {
+                    tv_xuanze.setSelected(true);
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
